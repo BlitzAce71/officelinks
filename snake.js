4e9d618faea2c9518fb9e195f9649f62c5e7be5e
BlitzAce71 <32716099+BlitzAce71@users.noreply.github.com>
2023-05-27 22:04:11 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ const gridSize = 50;
 const gridWidth = Math.floor(canvas.width / gridSize);
 const gridHeight = Math.floor(canvas.height / gridSize);
 
-let startingX, startingY, direction;
+//starting movement logic
 
 function checkInitialCollision(startingX, startingY, direction) {
   switch (direction) {
@@ -14,7 +14,7 @@ function checkInitialCollision(startingX, startingY, direction) {
     case 'right': return startingX === gridWidth - 1;
     default: return false;
   }
-}
+} while (isWallCollision(startingX, startingY, direction) || isSelfCollision(startingX, startingY, direction));
 
 function isWallCollision(x, y, direction) {
   switch (direction) {
@@ -35,6 +35,25 @@ function isSelfCollision(x, y, direction) {
   return false;
 }
 
+do {
+  startingX = Math.floor(Math.random() * (gridWidth - 2)) + 1; // Ensure a starting position away from the walls
+  startingY = Math.floor(Math.random() * (gridHeight - 2)) + 1;
+
+  if (startingX < gridWidth / 2) {
+    if (startingY < gridHeight / 2) {
+      direction = Math.random() < 0.5 ? 'right' : 'down';
+    } else {
+      direction = Math.random() < 0.5 ? 'right' : 'up';
+    }
+  } else {
+    if (startingY < gridHeight / 2) {
+      direction = Math.random() < 0.5 ? 'left' : 'down';
+    } else {
+      direction = Math.random() < 0.5 ? 'left' : 'up';
+    }
+  }
+} while (checkInitialCollision(startingX, startingY, direction) || isWallCollision(startingX, startingY, direction) || isSelfCollision(startingX, startingY, direction));
+
 function getNextHeadPosition(x, y, direction) {
   switch (direction) {
     case 'up': return { x, y: y - 1 };
@@ -44,7 +63,102 @@ function getNextHeadPosition(x, y, direction) {
   }
 }
 
+const snake = [
+  { x: startingX, y: startingY },
+];
+
+// Add the rest of the body based on the initial direction
+if (direction === 'left') {
+  snake.push({ x: startingX+1, y: startingY });
+  snake.push({ x: startingX+2, y: startingY });
+} else if (direction === 'right') {
+  snake.push({ x: startingX-1, y: startingY });
+  snake.push({ x: startingX-2, y: startingY });
+} else if (direction === 'up') {
+  snake.push({ x: startingX, y: startingY+1 });
+  snake.push({ x: startingX, y: startingY+2 });
+} else if (direction === 'down') {
+  snake.push({ x: startingX, y: startingY-1 });
+  snake.push({ x: startingX, y: startingY-2 });
+}
+
+let food = getRandomFoodPosition();
+let gameSpeed = 50;
+let headsEaten = 0;
+
+function getRandomFoodPosition() {
+  return {
+    x: Math.floor(Math.random() * gridWidth),
+    y: Math.floor(Math.random() * gridHeight)
+  };
+}
+
+function gameLoop() {
+  setTimeout(() => {
+    updateSnake();
+    renderSnake();
+    requestAnimationFrame(gameLoop);
+  }, gameSpeed);
+}
+
+// Shuffle array function
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+let faceIndices = Array.from({length: 17}, (_, i) => i + 1);
+shuffleArray(faceIndices);
+
+function updateSnake() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case 'up':    head.y -= 1; break;
+    case 'down':  head.y += 1; break;
+    case 'left':  head.x -= 1; break;
+    case 'right': head.x += 1; break;
+  }
+
+  if (head.x < 0 || head.x > gridWidth - 1 || head.y < 0 || head.y > gridHeight - 1) {
+    alert('Game Over');
+    resetGame();
+    return;
+  }
+
+  if (checkSnakeCollision(head)) {
+    alert('Game Over');
+    resetGame();
+    return;
+  }
+
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food = getRandomFoodPosition();
+    headsEaten++;
+    if (headsEaten === 15) {
+      alert('Congratulations! You win!');
+      resetGame();
+      return;
+    }
+  } else {
+    snake.pop();
+  }
+}
+
+function checkSnakeCollision(head) {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x === head.x && snake[i].y === head.y) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function resetGame() {
+let startingX, startingY, direction;
   snake.length = 0;
 
   do {
